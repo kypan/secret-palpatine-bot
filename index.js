cc1dbfecbcf05ab03e58787c33a121c4826bc952
Vincent Yang <vincent.yang@coursehero.com>
2017-08-03 19:12:10 -0700

    Added basic polling; alarms

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+var vote;
+var target_channel;
+var threshold;
+
 
 // Again, we define a port we want to listen to
 const PORT=4390;
@@ -22,7 +26,9 @@ app.listen(PORT, function () {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Example app listening on port " + PORT);
     //https://slack.com/api/users.list
-    
+    vote = 0;    
+    target_channel = 'C6HULCRQT'; // vincent-test for development
+        // channel = 'C5TA84QL8'; // always-loyalist
 });
 
 
@@ -31,38 +37,170 @@ app.get('/', function(req, res) {
     res.send('Ngrok is working! Path Hit: ' + req.url);
 });
 
-app.post('/', function(req, res) {
-    console.log(req.body);
-    var id = req.body.user_id;
-    var channel_id = req.body.channel_id;
-    console.log("channel id is " + channel_id);
-
+function getChannels() {
     var headers = {
         'User-Agent':       'Super Agent/0.0.1',
         'Content-Type':     'application/x-www-form-urlencoded'
     };
 
+    /* Look for all channels */
     var options = {
-        //url: 'https://slack.com/api/channels.info',
         url: 'https://slack.com/api/channels.list',
-        //url: 'https://slack.com/api/channels.info',
         method: 'POST',
         headers: headers,
         form: {
-            //'token': 'xoxp-2523418242-199931701171-222880851478-876149e7bc7126c08d74f4dfe7e1cd1b',
             'token': 'xoxp-2523418242-199931701171-222088546772-89b911e345c5b5c6559a383df8566e9d',
             'exclude_archived': 'true',
             'exclude_members': 'true'
-            //'channel':channel_id
         }
     };
     request(options, function(err, resp, body) {
-        //console.log(body);
-        console.log(body.channels);
-
+        if (err) {
+            console.log("Could not get all channels");
+        }
+        else {
+            return JSON.parse(body).channels;
+        }
     });
-    res.send('<@' + req.body.user_id + '>');
+}
+
+function getChannelInfo(channel) {
+    if (!channel) {
+        channel = target_channel;
+    }
+
+    var headers = {
+        'User-Agent':       'Super Agent/0.0.1',
+        'Content-Type':     'application/x-www-form-urlencoded'
+    };
+    /* Look for channel info */
+    var options = {
+        url: 'https://slack.com/api/channels.info',
+        method: 'POST',
+        headers: headers,
+        form: {
+            'token': 'xoxp-2523418242-199931701171-222088546772-89b911e345c5b5c6559a383df8566e9d',
+            'channel':channel
+        }
+    };
+    request(options, function(err, resp, body) {
+        return (body);
+    });
+}
+
+/* Reply a message to the user privately */
+function reply(req, res, msg) {
+    res.end(msg);
+}
+
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    var text = req.body.text;
+    var headers = {
+        'User-Agent':       'Super Agent/0.0.1',
+        'Content-Type':     'application/x-www-form-urlencoded'
+    };
+
+    inp = text.split(" ");
+
+
+    console.log(inp[0]);
+    console.log(Number.isInteger(parseInt(inp[1])));
+
+    if (inp[0] == 'start' && Number.isInteger(parseInt(inp[1]))) {
+        /* Is this a valid game? */
+        if (parseInt(inp[1]) <= 3) {
+            /* How many people do you want? */
+            if (Number.isInteger(parseInt(inp[2]))) {
+                threshold = parseInt(inp[2]);
+
+                /* How much time can you wait? */
+                if (Number.isInteger(parseInt(inp[3])) && parseInt(inp[3]) > -1) {
+                    //setInterval(function() {res.send("Time up!")}, parseInt(inp[3])); // need to send to the whole channel
+                    reply(req, res, "Starting game number " + inp[1] + " when we have " + threshold + " people within " + parseInt(inp[3]) + " minutes.");
+                }
+                reply(req, res, "Starting game number " + inp[1] + " when we have " + threshold + " people with no time limit.");
+            }
+            reply(req, res, "Missing number of people needed");
+        }
+        else {
+            reply(req, res, 'Invalid game number');
+        }
+    }
+    else if (inp[0] == 'reset') {
+        vote = 0;
+        threshold = 0;
+        res.end();
+    }
+    else if (text == "yes" || text == "y") {
+        vote = vote + 1;
+        // tell the channel what the current count is! 
+
+        if (vote > 0 && vote >= threshold) {
+            reply(req, res, "Start the game!");
+        }
+        console.log("Yes: " + vote);
+        res.end();
+    }
+    else if (text == "no" || text == 'n') {
+        console.log("Vote no");
+        res.end();
+    }
+    else if (text == 'help' || text == 'h') {
+        var msg = "/cmd start [game-num] [people-num] [time]";
+        reply(req, res, msg);
+    }
+    else if (text.substring(0, 5) == "start" || text.substring(0, 2) == 's ') {
+        console.log(text);
+        res.end();
+    }
+    else if (text == "games" || text == 'g') {
+        var msg = "Game List:\n1) Secret Palpatine\n2) Codenames\n3) Splendor";
+        sendToChannel(null, msg);
+    }
+    else if (text == "view" || text == 'v') {
+        var id = req.body.user_id;
+        var channel_id = req.body.channel_id;
+
+        var msg = "The vote is currently " + vote;
+        reply(req, res, msg);
+    }
+    else {
+        var id = req.body.user_id;
+        var channel_id = req.body.channel_id;
+        console.log("channel id is " + channel_id);
+
+        //res.send('<@' + req.body.user_id + '>');
+
+        var msg = 'Would you like to join a game <@' + req.body.user_id + '>?';
+        sendToChannel(channel_id, msg);
+    }
 });
+
+function sendToChannel(channel_id, msg) {
+    if (!channel_id) {
+        channel_id = target_channel;
+    }
+    var options = {
+        url: 'https://slack.com/api/chat.postMessage',
+        method: 'POST',
+        headers: headers,
+        form: {
+            'token': 'xoxp-2523418242-199931701171-222088546772-89b911e345c5b5c6559a383df8566e9d',
+            // this is the channel id for always-loyalist
+            'channel':target_channel,
+            'text': msg,
+            'username' : 'Game Maker'
+        }
+    };
+
+    request(options, function(err, resp, body) {
+        console.log(body);
+    });
+
+    res.end();
+}
 
 
 // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
